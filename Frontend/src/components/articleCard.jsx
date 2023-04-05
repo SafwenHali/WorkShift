54885b48126d0fc7e7c45ab479a9e47f6c7b7cab
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2023-04-05 02:24:32 +0200

    listArticles

--- Frontend/src/components/articleCard.jsx
+++ Frontend/src/components/articleCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 const Card =(props) =>{
     return(
-        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+        <div className="w-full md:w-1/2 lg:w-1/3 p-4 mb-4">
             <article className="overflow-hidden rounded-lg shadow-lg bg-white">
-                <img className="block h-80 w-full" 
+                <img className="block" 
                     src={props.imageURL}></img>
                 <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                     <h1 className="text-lg">
-                        <a className="no-underline hover:underline text-black hover:text-purple-700" href="#">
+                        <a className="no-underline hover:underline text-black hover:text-orange-700" href="#">
                             {props.title}
                         </a>
                     </h1>
