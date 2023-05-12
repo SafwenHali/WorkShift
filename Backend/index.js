ab452c6ce07bae50b42adfef2e2c5b486bd19ac6
aichajebri <77939143+aichajebri@users.noreply.github.com>
2023-05-12 10:29:10 +0100

    aawed auth

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,6 @@ const connectDB = require("./config/database");
 const cors = require("cors");
 const authConfig = require("./config/authConfig");
 const bodyParser = require("body-parser");
-const authMiddleware = require("./middlewares/authMiddleware");
-const authController = require("./controllers/authController");
-const userController = require("./controllers/userController");
 
 //.env
 require("dotenv").config();
@@ -36,22 +33,7 @@ app.use("/api/formation", require("./routes/formations"));
 app.use("/api/category", require("./routes/categories"));
 app.use("/api/subcategory", require("./routes/subcategories"));
 app.use("/api/cours", require("./routes/cours"));
-<<<<<<< Updated upstream
 
-=======
-app.use("/api/lessons", require("./routes/lessons"));
-// app.use("/api/auth", require("./routes/authRouter"));
-app.post("/api/auth/register", authController.register);
-app.post("/api/auth/login", authController.login);
-app.get("/api/users", [authMiddleware.verifyToken], userController.getAllUsers);
-app.get(
-  "/api/users/:userId",
-  [authMiddleware.verifyToken],
-  userController.getUserById
-);
-
-app.use("/api/Quizz", require("./Quizz/quizzRouter"));
->>>>>>> Stashed changes
 //server
 app.listen(process.env.PORT, () => {
   console.log("server started on", process.env.PORT);
