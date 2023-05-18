e621031b788078c6fa906743553c7ec2a0f97104
aichajebri <77939143+aichajebri@users.noreply.github.com>
2023-05-18 09:38:52 +0100

    bdit nraka7 fi b9iyet l auth

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,7 +3,10 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema(
   {
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userName: { type: String, required: true },
+
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: {
